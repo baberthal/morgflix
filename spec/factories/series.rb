02f7fca70d9cbeb6f88ebac46e33f63ce14bbddd
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-05 20:24:45 -0600

    Series Specs
    Create factory for generic series and initial validation specs.

--- spec/factories/series.rb
+++ spec/factories/series.rb
@@ -1,6 +1,5 @@
 FactoryGirl.define do
   factory :series do
-    
+    name { Faker::Book.title }
   end
-
 end
--- spec/models/series_spec.rb
+++ spec/models/series_spec.rb
@@ -1,5 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe Series, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:series) { build(:series) }
+
+  context 'with valid attributes' do
+    it 'is valid' do
+      expect(series).to be_valid
+    end
+  end
+
+  context 'with invalid attributes' do
+    before { series.name = nil }
+    it 'is invalid' do
+      expect(series).to_not be_valid
+    end
+  end
 end
