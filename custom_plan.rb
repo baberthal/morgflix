44ad1730fca8849a7deb067a0806f219261e5367
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-10 16:35:52 -0600

    Refactor Zeus CustomPlan
    Refactor custom_plan.rb to use some helper methods instead of one huge
    test method.

--- custom_plan.rb
+++ custom_plan.rb
@@ -7,9 +7,28 @@ class CustomPlan < Zeus::Rails
     require 'factory_girl'
     FactoryGirl.reload
     ENV['GUARD_RSPEC_RESULTS_FILE'] = 'tmp/guard_rspec_results.txt'
-    Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
-    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+    _load_all_files
     super
+  end
+
+  private
+
+  def _load_lib_files
+    Dir["#{Rails.root}/lib/**/*.rb"].each { |f| load f }
+  end
+
+  def _load_app_files
+    Dir["#{Rails.root}/app/**/*.rb"].each { |f| load f }
+  end
+
+  def _load_support_files
+    Dir[Rails.root.join('spec/support/**/*.rb')].each { |f| require f }
+  end
+
+  def _load_all_files
+    _load_lib_files
+    _load_app_files
+    _load_support_files
   end
 end
 
