0e278ce4a4b88f725978d3dc4a522c452a9840ed
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 01:06:17 -0600

    BrowserSync fixes

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@
 'use strict';
 
 var gulp = require('gulp'),
-    bs = require('./browser_sync'),
+    bs = require('browser-sync'),
     sass = require('gulp-sass'),
     sourcemaps = require('gulp-sourcemaps'),
     handleErrors = require('../util/handleErrors'),
@@ -11,9 +11,9 @@ var gulp = require('gulp'),
 
 gulp.task('sass', function() {
     return gulp.src(config.src)
-    .pipe(sourcemaps.init({ debug: true}))
+    .pipe(sourcemaps.init({debug: true}))
     .pipe(sass(config.settings))
-    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .pipe(autoprefixer({ browsers: config.browsers }))
     .on('error', handleErrors)
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(config.dest))
