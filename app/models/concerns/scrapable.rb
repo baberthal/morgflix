d4ffa3b8a73c3a077f6ff73a1c635b841d7da8c1
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 17:53:31 -0600

    Refactor Scrapable
    Some light refactoring for Scrapable.

--- app/models/concerns/scrapable.rb
+++ app/models/concerns/scrapable.rb
@@ -2,9 +2,11 @@ module Scrapable
   extend ActiveSupport::Concern
 
   def fetch_info(options = {})
-    info = SeriesSearcher.new.info(name, options)
-    info.transform_keys! { |k| k.snakeify.to_sym }
-    updated = self.attributes.select { |_, v| v.nil? }.symbolize_keys
+    info = SeriesSearcher.new.info(name, options).transform_keys! do |k|
+      k.to_s.snakeify.to_sym
+    end
+
+    updated = _nil_attrs
 
     updated.each_key { |k| updated[k] = info.fetch(_key_for(k)) }
 
@@ -12,6 +14,10 @@ module Scrapable
   end
 
   private
+
+  def _nil_attrs
+    attributes.select { |_, v| v.nil? }.symbolize_keys
+  end
 
   def _key_crosswalk
     {
