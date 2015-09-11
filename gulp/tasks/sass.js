f249afd8a26ad5f0c1e3fa8e3d62a6f1d8fa45b8
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 00:44:14 -0600

    Gulp Tasks and Config
    Config for browserify gulp tasks, add sourcemaps for sass task, and
    clean up the assets initializer.

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,21 +1,21 @@
 /* jslint node: true */
 'use strict';
 
-var gulp = require('gulp');
-var bs = require('./browser_sync');
-var sass = require('gulp-sass');
-// var sourcemaps = require('gulp-sourcemaps');
-var handleErrors = require('../util/handleErrors');
-var config = require('../config').sass;
-var autoprefixer = require('gulp-autoprefixer');
+var gulp = require('gulp'),
+    bs = require('./browser_sync'),
+    sass = require('gulp-sass'),
+    sourcemaps = require('gulp-sourcemaps'),
+    handleErrors = require('../util/handleErrors'),
+    config = require('../config').sass,
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     return gulp.src(config.src)
-        // .pipe(sourcemaps.init())
-        .pipe(sass(config.settings))
-        .on('error', handleErrors)
-        // .pipe(sourcemaps.write())
-        .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
-        .pipe(gulp.dest(config.dest))
-        .pipe(bs.stream());
+    .pipe(sourcemaps.init({ debug: true}))
+    .pipe(sass(config.settings))
+    .pipe(autoprefixer({ browsers: ['last 2 versions'] }))
+    .on('error', handleErrors)
+    .pipe(sourcemaps.write())
+    .pipe(gulp.dest(config.dest))
+    .pipe(bs.stream());
 });
