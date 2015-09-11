46740c1d556658608545abdfe8b8e260f9af6b7e
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 01:06:04 -0600

    Remove trailing blank line

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -3,4 +3,3 @@ Rails.application.config.assets.version = '1.0'
 public_assets = Rails.root.join('public', 'assets')
 Rails.application.config.assets.paths << public_assets.join('stylesheets')
 Rails.application.config.assets.paths << public_assets.join('javascripts')
-
