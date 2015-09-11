3ced10497949ebedc55ad4ab9fd4dc606f68e63a
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 17:50:01 -0600

    Spec Support
    Added dummy responses for TVDB api so as not to hammer their servers.
    Removed DeferredGarbageCollection entirely.

---
+++ spec/support/dummy_tvdb.rb
@@ -0,0 +1,25 @@
+module DummyTVDB
+  def dummy_archer_response
+    {
+      'seriesid' => '110381',
+      'language' => 'en',
+      'SeriesName' => 'Archer (2009)',
+      'banner' => 'graphical/110381-g8.jpg',
+      'Overview' => %(At ISIS, an international spy agency, global crises
+                      are merely opportunities for its highly trained
+                      employees to confuse, undermine, betray and royally
+                      screw each other. At the center of it all is suave
+                      master spy Sterling Archer, whose less-than-masculine
+                      code name is Duchess. Archer works with his
+                      domineering mother Malory, who is also his boss.
+                      Drama revolves around Archers ex-girlfriend, Agent
+                      Lana Kane and her new boyfriend, ISIS comptroller
+                      Cyril Figgis, as well as Malorys lovesick secretary,
+                      Cheryl.),
+      'FirstAired' => '2009-09-17',
+      'Network' => 'FXX',
+      'IMDB_ID' => 'tt1486217',
+      'zap2it_id' => 'EP01216702'
+    }
+  end
+end
