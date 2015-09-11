a484fea7f1496fffc7b538b7b51cb80c4740c767
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-11 16:42:08 -0600

    Bootstrap Class For
    Spec and implementation for bootstrap_class_for in the
    ApplicationHelper.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def bootstrap_classes
+    {
+      success: 'alert-success',
+      error: 'alert-danger',
+      alert: 'alert-warning',
+      notice: 'alert-info',
+      danger: 'alert-danger'
+    }
+  end
+
+  def bootstrap_class_for(flash_type)
+    bootstrap_classes.fetch(flash_type.to_sym) || flash_type.to_s
+  end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -13,24 +13,42 @@ RSpec.describe ApplicationHelper, type: :helper do
     end
   end
 
-  describe 'gulp_stylesheet_link_tag' do
+  describe '#gulp_stylesheet_link_tag' do
     it 'returns a link tag for the stylesheet specified' do
       expect(helper.gulp_stylesheet_link_tag('application'))
         .to match(%r{/assets/whatever/stylesheets/application})
     end
   end
 
-  describe 'gulp_stylesheet_link_tag' do
+  describe '#gulp_stylesheet_link_tag' do
     it 'returns a link tag for the javascript file specified' do
       expect(helper.gulp_javascript_include_tag('app'))
         .to match(%r{/assets/javascripts/app})
     end
   end
 
-  describe 'gulp_stylesheet_link_tag' do
+  describe '#gulp_stylesheet_link_tag' do
     it 'returns an anchor tag for the image specified' do
       expect(helper.gulp_image_tag('smiley'))
         .to match(%r{/assets/images/smiley})
+    end
+  end
+
+  describe '#bootstrap_class_for' do
+    let(:expected) do
+      {
+        success: 'alert-success',
+        error: 'alert-danger',
+        alert: 'alert-warning',
+        notice: 'alert-info',
+        danger: 'alert-danger'
+      }
+    end
+
+    it 'returns the proper class given the flash_type' do
+      expected.each do |k, v|
+        expect(helper.bootstrap_class_for(k)).to eq v
+      end
     end
   end
 end
