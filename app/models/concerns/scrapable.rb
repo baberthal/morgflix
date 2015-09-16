8ad9838495d00fc2869ff5865bb8ca0ab3ac5f30
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-16 17:10:23 -0600

    Refactor Scrapable
    Further refactoring. Reduced ABC size greatly.

--- app/models/concerns/scrapable.rb
+++ app/models/concerns/scrapable.rb
@@ -15,16 +15,26 @@ module Scrapable
   def info(options = {})
     full, banners, actors = TVDB.series_by_id(external_id, options)
     update!(full.app_attributes)
-    banners.each do |b|
+    _create_banners(banners) unless banners.nil? || banners.empty?
+    _create_actors(actors) unless actors.nil? || actors.empty?
+    true
+  end
+
+  private
+
+  def _create_banners(banner_array)
+    banner_array.each do |b|
       Banner.create!({
         series_id: Series.find_by(external_id: b.series_id).id
       }.merge(b.app_attributes))
-    end unless banners.nil?
-    actors.each do |a|
+    end
+  end
+
+  def _create_actors(actor_array)
+    actor_array.each do |a|
       Actor.create!({
         series_id: Series.find_by(external_id: a.series_id).id
       }.merge(a.app_attributes))
-    end unless banners.nil?
-    true
+    end
   end
 end
