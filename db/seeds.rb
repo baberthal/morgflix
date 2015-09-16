7a0bf1de53ff42abed94014fe51015ddfbe85834
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-16 17:11:02 -0600

    Remove seeds for now

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,26 +1,26 @@
-[Series].each do |model_class|
+[Series, Banner, Actor].each do |model_class|
   p "Destroying #{model_class.count} #{model_class}..."
   model_class.destroy_all
 end
 
-Series.create!(
-  [
-    {
-      name: 'Archer (2009)'
-    },
-    {
-      name: 'The Brink'
-    },
-    {
-      name: 'House'
-    },
-    {
-      name: 'Last Week Tonight'
-    },
-    {
-      name: 'Game of Thrones'
-    }
-  ]
-)
+# Series.create!(
+#   [
+#     {
+#       name: 'Archer (2009)'
+#     },
+#     {
+#       name: 'The Brink'
+#     },
+#     {
+#       name: 'House'
+#     },
+#     {
+#       name: 'Last Week Tonight'
+#     },
+#     {
+#       name: 'Game of Thrones'
+#     }
+#   ]
+# )
 
-p "Created #{Series.count} Series"
+# p "Created #{Series.count} Series"
