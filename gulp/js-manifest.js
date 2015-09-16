481ee99a2f5478f13e572b1c2f5a85eb1a753402
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-16 13:58:27 -0600

    Gulp Assets
    Added default banners for series without them. Added jquery ujs to
    application javascript manifest.

--- gulp/js-manifest.js
+++ gulp/js-manifest.js
@@ -6,7 +6,8 @@ var bowerFiles = './gulp/assets/bower_components';
 module.exports = {
     bowerComponents: [
         bowerFiles + '/jquery/dist/jquery.js',
-        bowerFiles + '/bootstrap/dist/js/bootstrap.js'
+        bowerFiles + '/bootstrap/dist/js/bootstrap.js',
+        bowerFiles + '/jquery-ujs/src/rails.js'
     ],
     headjs: [
         bowerFiles + '/webcomponentsjs/webcomponents-lite.js',
