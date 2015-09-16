c9d70253c329dba016b86f5bc5145b5ecddb2858
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-16 17:15:38 -0600

    Created Actor Factory

--- spec/factories/actors.rb
+++ spec/factories/actors.rb
@@ -1,6 +1,13 @@
 FactoryGirl.define do
   factory :actor do
-    
-  end
+    tvdb_actor_id '135751'
+    association :series, strategy: :build
 
+    trait :with_info do
+      name { Faker::Name.name }
+      role { Faker::Name.name }
+      image { "/actors/#{tvdb_actor_id}.jpg" }
+      sequence(:sort_order) { |n| n }
+    end
+  end
 end
