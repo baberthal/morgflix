3785493386efee4004063dc8609a057c7b6ee2c3
Morgan Lieberthal <morgan@jmorgan.org>
2015-09-17 10:54:53 -0600

    Update models to use sidekiq workers

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,4 +1,9 @@
 class Banner < ActiveRecord::Base
   belongs_to :series
   validates :series, presence: true
+  after_create :download
+
+  def download
+    BannerWorker.perform_async(id)
+  end
 end
--- app/models/concerns/scrapable.rb
+++ app/models/concerns/scrapable.rb
@@ -13,11 +13,14 @@ module Scrapable
   end
 
   def info(options = {})
+    SeriesInfoWorker.perform_async(external_id, options)
+  end
+
+  def worker_task(options = {})
     full, banners, actors = TVDB.series_by_id(external_id, options)
     update!(full.app_attributes)
     _create_banners(banners) unless banners.nil? || banners.empty?
     _create_actors(actors) unless actors.nil? || actors.empty?
-    true
   end
 
   private
@@ -25,7 +28,7 @@ module Scrapable
   def _create_banners(banner_array)
     banner_array.each do |b|
       Banner.create!({
-        series_id: Series.find_by(external_id: b.series_id).id
+        series_id: id
       }.merge(b.app_attributes))
     end
   end
@@ -33,7 +36,7 @@ module Scrapable
   def _create_actors(actor_array)
     actor_array.each do |a|
       Actor.create!({
-        series_id: Series.find_by(external_id: a.series_id).id
+        series_id: id
       }.merge(a.app_attributes))
     end
   end
